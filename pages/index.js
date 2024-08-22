9c0600a1a9444a626388c48ca0e4bdee6a52c653
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2024-08-22 11:17:27 +0000

    Initial Home page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import { useCallback, useEffect, useState } from 'react'
 import Button from '../components/Button'
 import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
+import React from 'react';
+import { MdEditor } from 'md-editor-rt';
+import 'md-editor-rt/lib/style.css';
+
 
 function throwError() {
   console.log(
@@ -11,56 +15,35 @@ function throwError() {
 }
 
 function Home() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
 
-  useEffect(() => {
-    const r = setInterval(() => {
-      increment()
-    }, 1000)
-
-    return () => {
-      clearInterval(r)
-    }
-  }, [increment])
-
+  const [text, setText] = useState('# Hello Editor\nThis is your workspace.');
   return (
     <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
-      <p>
-        Fast Refresh is a Next.js feature that gives you instantaneous feedback
-        on edits made to your React components, without ever losing component
-        state.
-      </p>
+      <h1>EasyPea</h1>
+
       <hr className={styles.hr} />
+      <h4>Editor</h4>
+      <p>
+        This is your workspace. 
+      </p>
       <div>
-        <p>
-          Auto incrementing value. The counter won't reset after edits or if
-          there are errors.
-        </p>
-        <p>Current value: {count}</p>
+      <MdEditor preview={
+        !false
+      } language={'en-US'} modelValue={text} onChange={setText} />
       </div>
       <hr className={styles.hr} />
       <div>
-        <p>Component with state.</p>
+        <p>Fidget with this button :)</p>
         <ClickCount />
       </div>
       <hr className={styles.hr} />
       <div>
-        <p>
-          The button below will throw 2 errors. You'll see the error overlay to
-          let you know about the errors but it won't break the page or reset
-          your state.
-        </p>
         <Button
           onClick={(e) => {
-            setTimeout(() => document.parentNode(), 0)
-            throwError()
+            // nothing
           }}
         >
-          Throw an Error
+          Get. Set. Go.
         </Button>
       </div>
       <hr className={styles.hr} />
